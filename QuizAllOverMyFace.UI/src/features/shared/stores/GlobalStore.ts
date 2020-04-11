f362c387b43c24cfe3e40375bf1cb8dbb9989c41
Michael Forde <MichaelFo@datacom.co.nz>
2020-04-12 11:32:41 +1200

    Added Global Store

---
+++ QuizAllOverMyFace.UI/src/features/shared/stores/GlobalStore.ts
@@ -0,0 +1,114 @@
+import { observable, action } from "mobx"
+import { createContext } from "react"
+import React from "react"
+import TimelineIcon from '@material-ui/icons/Timeline'
+import { cloneObject } from "shared-components/utils/utils"
+// import { UserLoginResponse } from "client/backendclient"
+//import { userCacheHandler } from "client/auth"
+import ResetPassword from "features/reset-password/ResetPassword";
+
+interface IMenuItem {
+    path: string
+    name: string
+    exact: boolean
+    landingPage: boolean;
+    component: React.ComponentType
+    icon: React.ComponentType
+    displaysInMenu: boolean
+}
+export interface IMenuItemGroup {
+    divisionName: string
+    icon?: string
+    routes: IMenuItem[]
+}
+
+const defaultMenuItems: IMenuItemGroup[] = [
+    {
+        divisionName: "Reports",
+        routes: [
+            {
+                landingPage: false, exact: true, displaysInMenu: false, 
+                name: "Reset Password", path: "/reset-password/:guid/:userId",
+                component: ResetPassword,
+                icon: TimelineIcon
+            },
+            {
+                landingPage: false, exact: true, displaysInMenu: false, 
+                name: "Reset Password", path: "/reset-password/",
+                component: ResetPassword,
+                icon: TimelineIcon
+            },
+            // {
+            //     landingPage: false, exact: false, displaysInMenu: false, 
+            //     name: "Reset Password", path: "/reset-password/",
+            //     component: ResetPassword,
+            //     icon: TimelineIcon
+            // },
+        ],
+    },
+    // {
+    //     divisionName: "",
+    //     routes: [
+    //         {
+    //             landingPage: false, exact: false, displaysInMenu: false, 
+    //             name: "Reset Password", path: "/reset-password/",
+    //             component: ResetPassword,
+    //             icon: TimelineIcon
+    //         },
+    //     ],
+    // },
+]
+
+export class GlobalStore {
+
+    constructor(load: boolean = true) {
+        if (load) {
+            this.renderMenuItems()
+        }
+    }
+
+    @observable menuItems: IMenuItemGroup[] = []
+    
+    //@observable currentUser? = userCacheHandler.get()
+    // @action logout = () => {
+    //     this.currentUser = undefined
+    //     userCacheHandler.set(undefined)
+    // }
+    // @action login = (user: UserLoginResponse) => {
+    //     userCacheHandler.set(user)
+    //     this.currentUser = user
+    // }
+
+    @action private renderMenuItems = () => {
+        // ** this can be used to render the menu from an api call
+        let newMenuItems: IMenuItemGroup[] = []
+
+        for (const defaultMenuItemGroup of defaultMenuItems) {
+            const menuItemGroup = cloneObject(defaultMenuItemGroup)
+            menuItemGroup.routes = []
+            for (const defaultRoute of defaultMenuItemGroup.routes) {
+                if (defaultRoute.landingPage) {
+                    const landing = cloneObject(defaultRoute)
+                    landing.path = '/'
+                    landing.exact = true
+                    landing.displaysInMenu = false
+                    landing.component = defaultRoute.component
+                    landing.icon = defaultRoute.icon
+                    menuItemGroup.routes.push(landing)
+                }
+                const menuItem = cloneObject(defaultRoute)
+                menuItem.component = defaultRoute.component
+                menuItem.icon = defaultRoute.icon
+                menuItemGroup.routes.push(menuItem)
+            }
+            if (menuItemGroup.routes.length > 0) {
+                newMenuItems.push(menuItemGroup)
+            }
+        }
+        this.menuItems = newMenuItems
+    }
+    /** defines if the menu drawer is open */
+    @observable drawerOpen = false
+}
+
+export const GlobalStoreContext = createContext(new GlobalStore(false)) // this is to be accessed by the react components
